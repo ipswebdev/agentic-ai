e7d8dde946959733ab2547d1d7d0eea33025208c
pranay sawant <sawant.pranay.97@gmail.com>
2026-07-06 17:29:43 +0530

    added changes to readme.md

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -4,7 +4,7 @@ import ChatWrapper from "../components/chat/ChatWrapper";
 
 export default async function Chat() {
   const res =  await fetchUserDocuments();
-  const fetchedDocs = res['documents']?.length ?  res['documents'].map(d => d) : []
+  const fetchedDocs = res['documents']?.length ?  res['documents'].map(d => d) : [];
   return (
     <div className="min-h-screen bg-zinc-900 text-zinc-100">
       <Header></Header>
